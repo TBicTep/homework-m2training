c70825da32f4f6e4b2594a3b4134217be361a086
TBicTep <tbictep@gmail.com>
2021-10-07 21:07:59 +0300

    2.2

---
+++ app/code/Training/Test/Model/App/FrontController.php
@@ -0,0 +1,42 @@
+<?php
+namespace Training\Test\Model\App;
+
+class FrontController extends \Magento\Framework\App\FrontController
+{
+    /**
+    * @var \Magento\Framework\App\RouterListInterface
+    */
+    protected $_routerList;
+    /**
+    * @var \Magento\Framework\App\ResponseInterface
+    */
+    protected $response;
+    /**
+    * @var \Psr\Log\LoggerInterface
+    */
+    private $logger;
+    /**
+    * @param \Magento\Framework\App\RouterListInterface $routerList
+    * @param \Magento\Framework\App\ResponseInterface $response
+    * @param \Psr\Log\LoggerInterface $logger
+    */
+    public function __construct(
+        \Magento\Framework\App\RouterListInterface $routerList,
+        \Magento\Framework\App\ResponseInterface $response,
+        \Psr\Log\LoggerInterface $logger
+    ) {
+        $this->_routerList = $routerList;
+        $this->response = $response;
+        $this->logger = $logger;
+        parent::__construct($routerList, $response);
+    }
+    public function dispatch(\Magento\Framework\App\RequestInterface $request)
+    {
+        foreach ($this->_routerList as $router) {
+            $this->logger->info(get_class($router));
+        }
+        return parent::dispatch($request);
+    }
+}
+
+?>
